1b563bfa16fcfb83042a939b6c2e8f36eb46717f
Dmitriy Aleksyutin <aleksyutindo@gmail.com>
2025-09-29 21:02:27 +0300

    Реализовано чтение табличных файлов (*.xlsx, *.xls)
    Реализован показ ошибки при некорректном расширении файла
    Реализована эмуляция поиска названий в поисковой строке на сайте

--- src/gui/fill_excel_columns_module.py
+++ src/gui/fill_excel_columns_module.py
@@ -1,6 +1,15 @@
 """
 """
 
+import pandas as pd
+import time
+from selenium import webdriver as wd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as WDW
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager as CDM
+from bs4 import BeautifulSoup as BS
 from PySide6.QtWidgets import (
     QWidget, QVBoxLayout, QLabel, QPushButton, QFileDialog, QMessageBox
 )
@@ -46,15 +55,68 @@ class FillExcelColumns(QWidget):
             if not self.check_file_extensions(file_path):
                 QMessageBox.warning(self, "Ошибка", "Неподдерживаемый тип файла!")
             else:
+                self.time_save_file(file_path)
                 print(f"File dropped: {file_path}")
 
     def browse_file(self):
         file_paths, _ = QFileDialog.getOpenFileNames(self, "Select files", "", "Excel-files (*.xlsx; *.xls)")
         for file_path in file_paths:
             print(f"File selected: {file_path}")
+            self.time_save_file(file_path)
 
     def check_file_extensions(self, file_path):
         return file_path.endswith(('.xlsx', '.xls'))
-    
-    def parse_excel_data():
-        pass
+
+    def time_save_file(self, file_path):
+        try:
+            self.df = pd.read_excel(file_path)
+            self.parse_excel_data()
+        except Exception as e:
+            QMessageBox.warning(self, "Ошибка", f'Не удалось обработать файл: {str(e)}')
+
+    def parse_excel_data(self):
+        data_fourth_column = self.analyze_file()
+        print("Данные из четвертого столбца:")
+        for el in data_fourth_column:
+            print(el)
+        self.inner_parse(data_fourth_column)
+
+    def analyze_file(self):
+        try:
+            data = []
+            column_index = self.df.columns.get_loc("Образовательное учреждение из 1С")
+            column_data = self.df.iloc[0:, column_index].dropna()
+            data = column_data.tolist()
+            return data
+        except Exception as e:
+            QMessageBox.warning(self, "Ошибка", f'Не удалось проанализировать файл: {str(e)}')
+
+    def inner_parse(self, our_parse_data):
+        # driver = CDM().install()
+        chrome_options = wd.ChromeOptions()
+        chrome_options.add_argument("--log-level=3")
+        browser = wd.Chrome(options=chrome_options)
+        browser.get("https://www.rusprofile.ru/")
+        time.sleep(2)
+        open_search = browser.find_element(By.ID, "indexsearchform")
+        open_search.click()
+        search = browser.find_elements(By.NAME, "query")[1]
+        time.sleep(2)
+        # time.sleep(2)
+        for data_element in our_parse_data:
+            search.send_keys(data_element)
+            time.sleep(3)
+            # search = browser.find_elements(By.NAME, "query")[1]
+            search.send_keys(Keys.CONTROL + 'a')
+            search.send_keys(Keys.DELETE)
+            # search.send_keys(Keys.ENTER)
+        time.sleep(2)
+        browser.quit()
+        # time.sleep(2)
+        # open_search = browser.find_element(By.ID, "indexsearchform")
+        # open_search.click()
+        # time.sleep(2)
+        # search = browser.find_elements(By.NAME, "query")[1]
+        # # browser.execute_script("arguments[0].send_keys('Школа 58');", search)
+        # search.send_keys("Школа 58")
+        # search.send_keys(Keys.ENTER)
