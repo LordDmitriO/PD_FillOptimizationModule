edb134958984219c3353310c2619afab5cbfa9c0
Dmitriy Aleksyutin <aleksyutindo@gmail.com>
2025-12-10 05:01:56 +0300

    Небольшие правки

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -110,26 +110,6 @@ class MainWindow(QMainWindow):
         self.is_dev_mode = new_settings["dev_mode"]
         config.UserAppSettings.is_dev_mode = new_settings["dev_mode"]
 
-    def closeEvent(self, event):
-        """Обработка закрытия главного окна"""
-        # Корректно завершаем все потоки во вкладках
-        for i in range(self.tab_widget.count()):
-            widget = self.tab_widget.widget(i)
-            if hasattr(widget, 'stop_parsing') and widget.is_parsing:
-                # Останавливаем парсинг если он выполняется
-                widget.stop_parsing()
-            if hasattr(widget, 'parser_thread') and widget.parser_thread:
-                if widget.parser_thread.isRunning():
-                    widget.parser_thread._stop_requested = True
-                    if widget.parser_thread.parser:
-                        try:
-                            widget.parser_thread.parser.close_browser()
-                        except:
-                            pass
-                    widget.parser_thread.wait(3000)
-        
-        event.accept()
-
     def set_language(self):
         pass
 
